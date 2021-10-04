50ca21b0ba133f55e25ff1b7249728b8fb887758
Johan Larsson <johan.larsson@edu.edugrade.se>
2021-10-04 22:55:00 +0200

    add

--- Labb2/Labb2/Boats.cs
+++ Labb2/Labb2/Boats.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Labb2
 {
-    class Boats
+    public class Boat
     {
+        public int Speed { get; set; }
+
+        public Boat(int speed)
+        {
+            Speed = speed;
+        }
+
+        public static List<int> listOfBoats = new List<int>();
+
+        public static void CreateList(int input)
+        {
+
+            var random = new Random();
+
+            for (int i = 0; i < input; i++)
+            {
+                listOfBoats.Add(random.Next(10, 100));
+            }
+        }
     }
 }
