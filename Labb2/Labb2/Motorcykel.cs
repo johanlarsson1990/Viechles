991e80d414083b862467fbe7d2f72871cea3705a
Johan Larsson <johan.larsson@edu.edugrade.se>
2021-10-04 22:53:04 +0200

    add

--- Labb2/Labb2/Motorcykel.cs
+++ Labb2/Labb2/Motorcykel.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Labb2
 {
-    class Motorcykel
+    public class Motorcycle
     {
+        public int Speed { get; set; }
+
+        public Motorcycle(int speed)
+        {
+            Speed = speed;
+        }
+
+        public static List<int> listOfMotorcycles = new List<int>();
+        public static void CreateList(int input)
+        {
+
+            var random = new Random();
+
+            for (int i = 0; i < input; i++)
+            {
+                listOfMotorcycles.Add(random.Next(10, 100));
+            }
+        }
     }
 }
