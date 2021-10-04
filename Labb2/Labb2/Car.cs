a4196877678034624e876cf876c2ce8d5a234446
Oliver Rodin <oliver.rodin@edu.edugrade.se>
2021-10-04 15:51:30 +0200

    Grund

---
+++ Labb2/Labb2/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb2
+{
+    public class Car
+    {
+        public int Speed { get; set; }
+
+
+        public static List<int> listOfCars = new List<int>();
+
+
+        public Car()
+        {
+
+        }
+
+        public static void CreateList(int input)
+        {
+
+            var random = new Random();
+
+
+
+            for (int i = 0; i < input; i++)
+            {
+
+                listOfCars.Add(random.Next(10, 100));
+
+            }
+        }
+    }
+}
---
+++ Labb2/Labb2/Motorcycle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb2
+{
+    public class Motorcycle
+    {
+        public int Speed { get; set; }
+
+        public Motorcycle(int speed)
+        {
+            Speed = speed;
+        }
+
+        public static List<int> listOfMotorcycles = new List<int>();
+        public static void CreateList(int input)
+        {
+
+            var random = new Random();
+
+            for (int i = 0; i < input; i++)
+            {
+                listOfMotorcycles.Add(random.Next(10, 100));
+            }
+        }
+    }
+}
