5fb4538240a6d56d878925df0fc20cc969449501
Elijas Andersson <elijas.andersson@edu.edugrade.se>
2021-10-06 08:13:48 +0200

    test2

---
+++ Labb2/Labb2/Boat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb2
+{
+    public class Boat
+    {
+        public int Speed { get; set; }
+
+        public Boat(int speed)
+        {
+            Speed = speed;
+        }
+
+        public static List<int> listOfBoats = new List<int>();
+
+        public static void CreateList(int input)
+        {
+
+            var random = new Random();
+
+            for (int i = 0; i < input; i++)
+            {
+                listOfBoats.Add(random.Next(10, 100));
+            }
+        }
+    }
+}
