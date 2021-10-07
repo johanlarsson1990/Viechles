83e9a095225ec1e46f466c36044809565b5e07be
Johan Larsson <johan.larsson@edu.edugrade.se>
2021-10-07 12:50:12 +0200

    add

--- Labb2/Labb2/Program.cs
+++ Labb2/Labb2/Program.cs
@@ -23,32 +23,7 @@ namespace Labb2
         }
         public static void Menu()
         {
-            
-            Console.WriteLine("----Welcome to Vehicles!----\n" +
-                              "You're going to create an optional number of three specific\n" +
-                              "vehicle types, Cars, Boats and Motorcycles.\n" +
-                              "Ready? Please press enter!");
-            Console.ReadLine();
-            Console.Clear();
-
-
-            Console.WriteLine("How many Cars do you want to create?");
-            //AddVehicle(int.Parse(Console.ReadLine()), Vehicle.Car);
-            //Car.CreateList(int.Parse(Console.ReadLine()));
-            AddVehicle(int.Parse(felhantering(Console.ReadLine())), Vehicle.Car);
-            Console.Clear();
-
-            Console.WriteLine("How many Boats do you want to create?");
-            AddVehicle(int.Parse(felhantering(Console.ReadLine())), Vehicle.Boat);
-            //Boat.CreateList(int.Parse(Console.ReadLine()));
-            Console.Clear();
-
-            Console.WriteLine("How many Motorcycles do you want to create?");
-            AddVehicle(int.Parse(felhantering(Console.ReadLine())), Vehicle.Motorcycle);
-            //Motorcycle.CreateList(int.Parse(Console.ReadLine()));
-            Console.Clear();
-
-
+            Welcome();
 
             // while
             while (true)
@@ -58,7 +33,7 @@ namespace Labb2
                                   "2. Print/Create Boats\n" +
                                   "3. Print/create Motorcycles\n" +
                                   "4. Print all vehicles in m/s");
-                input = int.Parse(felhantering(Console.ReadLine(),4,true));
+                errorHandeling(Console.ReadLine(),null,true);
                 Console.Clear();
 
                 switch (input)
@@ -164,21 +139,52 @@ namespace Labb2
             }
 
         }
+        public static void Welcome ()
+        {
+            Console.WriteLine("----Welcome to Vehicles!----\n" +
+                              "You're going to create an optional number of three specific\n" +
+                              "vehicle types, Cars, Boats and Motorcycles.\n" +
+                              "Ready? Please press enter!");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("How many Cars do you want to create?");
+            errorHandeling(Console.ReadLine());
+            AddVehicle(input, Vehicle.Car);
+            //AddVehicle(int.Parse(Console.ReadLine()), Vehicle.Car);
+            //AddVehicle(int.Parse(felhantering(Console.ReadLine())), Vehicle.Car);
+            Console.Clear();
+
+            Console.WriteLine("How many Boats do you want to create?");
+            errorHandeling(Console.ReadLine());
+            AddVehicle(input, Vehicle.Boat);
+            Console.Clear();
+
+            Console.WriteLine("How many Motorcycles do you want to create?");
+            errorHandeling(Console.ReadLine());
+            AddVehicle(input, Vehicle.Motorcycle);
+            Console.Clear();
+        }
         public static void WriteList(List<IVehicle> list, string vehicle, string speedUnit)
         {
 
             Console.WriteLine($"-- {list.Count} {vehicle}s in stock --");
-
-            foreach (var i in list)
-                Console.WriteLine($"{i.Name} - {i.getSpeed()} {speedUnit}");
+            for (int i = 0; i < list.Count; i++)
+            {
+                var index = i+1;
+                Console.WriteLine($"{index}. {list[i].Name} - {list[i].getSpeed()} {speedUnit}");
+            }
+            //foreach (var i in list)
+            //    Console.WriteLine($"{i.Name} - {i.getSpeed()} {speedUnit}");
 
             Console.WriteLine($"----------------");
             Console.WriteLine($"Please select {vehicle} to change (1-{list.Count}) or enter + to add a new {vehicle}");
-            inputString = felhantering(Console.ReadLine());
-            if (inputString != "+")
-            {
-                Item = list[int.Parse(felhantering(inputString, list.Count,true)) - 1];
-            }
+            //inputString = felhantering(Console.ReadLine());
+            errorHandeling(Console.ReadLine(),list,false,true);
+            //if (inputString != "+")
+            //{
+            //    Item = list[int.Parse(felhantering(inputString, list.Count,true)) -1];
+            //}
             Console.Clear();
         }
 
@@ -205,13 +211,14 @@ namespace Labb2
 
         public static void ShowSpecificVehicle(IVehicle Item, string speedUnit)
         {
-            input = int.Parse(felhantering(inputString));
-
+            //input = int.Parse(felhantering(inputString));
+            errorHandeling(inputString);
             Console.WriteLine($"-- {Item.Name} --");
             Console.WriteLine($"Speed: {Item.getSpeed()} {speedUnit}");
             Console.WriteLine("----------------");
             Console.WriteLine($"Please enter a new speed(0-100) or - to remove {Item.Name}");
-            inputString = felhantering(Console.ReadLine());
+            //inputString = felhantering(Console.ReadLine());
+            errorHandeling(Console.ReadLine(),null,true,true);
             //if (inputString != "-")
             //    input = int.Parse(felhantering(inputString));
             Console.Clear();
@@ -257,35 +264,45 @@ namespace Labb2
             }
             Console.WriteLine($"{vehicletoprint.Name} - {ms} - m/s");
         }
-
-        public static string felhantering(string error, int count=0,bool list =false)
+        public static void errorHandeling(string error, List<IVehicle> fordon = null,bool menyOrRemove=false,bool add=false)
         {
-            
             int output = 0;
             bool wrong = true;
             while (wrong)
             {
-                if(error == "-" || error == "+")
-                {
-                    wrong = false;
-                    break;
-                }
                 var test = int.TryParse(error, out output);
-                if (test == false && list == false)
+                if (menyOrRemove && error == "-" && add || error == "+" && add && menyOrRemove == false)
                 {
-                    Console.WriteLine("Wrong input, try again");
-                    error = Console.ReadLine();
+                    inputString = error;
+                    wrong = false;
+
                 }
-                else if (test == false && count > 0 || list == true && test == true && output > count)
+                else if (fordon != null && output >= 0 && output < fordon.Count && test == true)
                 {
-                    Console.WriteLine("Wrong input, try again");
-                    error = Console.ReadLine();
+                    Item = fordon[output-1];
+                    inputString = error;
+                    wrong = false;
+                }
+                else if (menyOrRemove && output >= 1 && output < 4)
+                {
+                    input = output;
+                    wrong = false;
+                }
+                else if(test && output >= 0 && output < 100 && fordon == null)
+                {
+                    input = output;
+                    inputString = error;
+                    wrong = false;
                 }
                 else
-                    wrong = false;
+                {
+                    Console.WriteLine("Wrong input, try again");
+                    error = Console.ReadLine();
+                }
+                
             }
-            return error;
         }
+    
       
         // Samma funktion som print+case4 på ett ställe, mer lättläsligt!!!!!!!!!!!!!!!!!
         // Caset sen mycket "cleanare" ut med koden nedanför.
